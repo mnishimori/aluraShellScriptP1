1d133de50f95d8f73b8adab0b4a245810bd4f116
mnishimori <mnishimori@yahoo.com.br>
2022-02-17 08:43:57 -0300

    Passando parâmetros para um ShellScript

--- conversao-jpg-png.sh
+++ conversao-jpg-png.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-convert ../imagens-livros/$1.jpg ../imagens-livros/$1.png
+
+CAMINHO_IMAGENS=../imagens-livros
+
+for imagem in $@
+do
+	convert $CAMINHO_IMAGENS/$imagem.jpg $CAMINHO_IMAGENS/$imagem.png
+done
